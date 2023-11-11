63230a7081156557a276fe9463f0e01d85e4bdf5
Grufx <aleksuser3@gmail.com>
2023-11-11 22:22:30 +0200

    Homeworks 10 lesson

--- src/main/java/automation/AliexpressTest.java
+++ src/main/java/automation/AliexpressTest.java
@@ -15,16 +15,18 @@ public class AliexpressTest {
         WebDriver driver = new FirefoxDriver();
         driver.get("https://aliexpress.com/");
         String searchText = "iphone 14 pro max case";
+
         sleep(500);/*need a timer for the site to load and for ads to appear, which the test will close*/
         driver.findElement(By.xpath("//img[@class = 'pop-close-btn']")).click(); /*Close ad*/
         driver.findElement(By.xpath("//button[@class = 'btn-accept']")).click(); /*Accept all cookies*/
 
-        WebElement search = driver.findElement(By.xpath("//*[@id=\"search-words\"]"));
+        WebElement search = driver.findElement(By.xpath("//input[@id=\"search-words\"]"));
         search.sendKeys(searchText);
-        WebElement searchButton = driver.findElement(By.xpath("//*[@id=\"_global_header_23_\"]/div/div/div[2]/div[1]/div[1]/div/input[2]"));
-        searchButton.click();
-        List<WebElement> resultLinks = driver.findElements(By.xpath("//h1[@class = 'multi--titleText--nXeOvyr']"));
 
+        WebElement searchButton = driver.findElement(By.xpath("//input[@class = 'search--submit--2VTbd-T']"));
+        searchButton.click();
+
+        List<WebElement> resultLinks = driver.findElements(By.xpath("//h1[@class = 'multi--titleText--nXeOvyr']"));
         System.out.println("Number of links found: "+ resultLinks.size());
 
         for (WebElement title : resultLinks) {
